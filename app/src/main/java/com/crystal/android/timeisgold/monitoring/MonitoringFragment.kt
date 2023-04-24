f4db11ddb7c77f1465307ad6873897ef59258bf4
박수정 <zxzx4342@naver.com>
2023-04-24 22:58:47 +0900

    백업용

--- app/src/main/java/com/crystal/android/timeisgold/monitoring/MonitoringFragment.kt
+++ app/src/main/java/com/crystal/android/timeisgold/monitoring/MonitoringFragment.kt
@@ -6,13 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.crystal.android.timeisgold.R
 import com.crystal.android.timeisgold.databinding.FragmentMonitoringBinding
+import com.crystal.android.timeisgold.record.RecordViewModel
+import com.crystal.android.timeisgold.util.ContextUtil
 
 class MonitoringFragment: Fragment() {
 
     private var _binding: FragmentMonitoringBinding? = null
     private val binding get() = _binding!!
+
+    private val recordViewModel: RecordViewModel by lazy {
+        ViewModelProvider(this).get(RecordViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,13 +28,13 @@ class MonitoringFragment: Fragment() {
     ): View? {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_monitoring, container, false)
 
-        setValues()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setValues()
         setupEvents()
     }
 
@@ -38,10 +45,18 @@ class MonitoringFragment: Fragment() {
 
     private fun setValues() {
 
+        val itemList = ContextUtil.getTypeListPref(requireContext())
+        recordViewModel.recordDailyLiveData.observe(viewLifecycleOwner) {
+            it?.let {
+              //   updateUI(itemList, it)
+            }
+        }
+
     }
 
     private fun setupEvents() {
 
     }
+
 
 }
